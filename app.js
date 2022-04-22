ca36b5886c1eb1f4988584facee60b8e39a98215
mischa.huang <mischa.huang@creativearktech.com>
2022-04-22 17:44:28 +0800

    feat: insert data, .create()
    api: [POST] /posts

--- app.js
+++ app.js
@@ -5,41 +5,46 @@ mongoose.connect('mongodb://localhost:27017/postDB')
   .then(() => console.log('db connect success'))
   .catch(e => console.log(e));
 
-const PostsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'name field required']
+const PostsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'name field required']
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    content: {
+      type: String,
+      required: [true, 'content field required']
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    comments: {
+      type: Number,
+      default: 0
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    type: {
+      type: String,
+      enum:['group', 'person'],
+      required: [true, 'type field required']
+    },
+    tags: {
+      type: [String],
+      required: [true, 'tags field required']
+    }
   },
-  image: {
-    type: String,
-    default: '',
-  },
-  content: {
-    type: String,
-    required: [true, 'content field required']
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-  comments: {
-    type: Number,
-    default: 0
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  type: {
-    type: String,
-    enum:['group', 'person'],
-    required: [true, 'type field required']
-  },
-  tags: {
-    type: [String],
-    required: [true, 'tags field required']
+  {
+    versionKey: false,
   }
-});
+);
 const Post = mongoose.model('Post', PostsSchema);
 
 const headers = {
@@ -53,6 +58,11 @@ const requestListener = async (req, res) => {
   const { url, method } = req;
   const data = await Post.find();
 
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk
+  });
+
   if(url === '/posts' && method === 'GET') {
     res.writeHead(200, headers);
     res.write(JSON.stringify({
@@ -60,6 +70,38 @@ const requestListener = async (req, res) => {
       data
     }));
     res.end();
+  } else if(url === '/posts' && method === 'POST') {
+    req.on('end', async () => {
+      try {
+        const data = JSON.parse(body);
+        const { name, image, content, type, tags } = data;
+
+        const newPost = await Post.create({
+          name,
+          image,
+          content,
+          type,
+          tags
+        });
+        res.writeHead(200, headers);
+        res.write(JSON.stringify({
+          'status': 'success',
+          data: newPost
+        }));
+
+        res.end();
+      } catch (e) {
+        const errorMsg = e.message || 'parse error.';
+
+        res.writeHead(400, headers);
+        res.write(JSON.stringify({
+          'status': 'false',
+          'message': errorMsg
+        }));
+        
+        res.end();
+      };
+    });
   } else if(method === 'OPTIONS') {
     res.writeHead(200, headers);
     res.end();
