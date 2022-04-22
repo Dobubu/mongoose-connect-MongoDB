8b30708d299ff9a493cf05f37a4537eb976fe4fa
mischa.huang <mischa.huang@creativearktech.com>
2022-04-22 17:09:14 +0800

    feat: add Post Model

--- app.js
+++ app.js
@@ -5,6 +5,43 @@ mongoose.connect('mongodb://localhost:27017/postDB')
   .then(() => console.log('db connect success'))
   .catch(e => console.log(e));
 
+const PostsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'name field required']
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  content: {
+    type: String,
+    required: [true, 'content field required']
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  comments: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  type: {
+    type: String,
+    enum:['group', 'person'],
+    required: [true, 'type field required']
+  },
+  tags: {
+    type: [String],
+    required: [true, 'tags field required']
+  }
+});
+const Post = mongoose.model('Post', PostsSchema);
+
 const requestListener = (req, res) => {
   res.writeHead(200, {'Content-type': 'text/plain'});
   res.write('hello');
